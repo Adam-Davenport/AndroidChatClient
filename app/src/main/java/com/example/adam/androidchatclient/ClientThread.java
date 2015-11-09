bb1ccd82557dbd6c8d9fdb8599940e097e73746e
cabewz <cabewz@gmail.com>
2015-11-08 23:28:10 -0500

    minor upate

--- app/src/main/java/com/example/adam/androidchatclient/ClientThread.java
+++ app/src/main/java/com/example/adam/androidchatclient/ClientThread.java
@@ -4,9 +4,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
-/**
- * Created by Adam on 11/8/2015.
- */
 public class ClientThread implements Runnable {
 
     Thread runner;
