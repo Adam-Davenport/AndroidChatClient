237045ec206130a1c8a3ba43d1c2c7d7a0470cc1
cabewz <cabewz@gmail.com>
2015-11-19 12:45:06 -0500

    Asynchronous implementation

--- app/src/main/java/com/example/adam/androidchatclient/MainActivity.java
+++ app/src/main/java/com/example/adam/androidchatclient/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.adam.androidchatclient;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
+    private Socket client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +26,46 @@ public class MainActivity extends AppCompatActivity {
 
     public void connect(View v)
     {
-        ClientThread thread = new ClientThread();
-        thread.run();
+        SendMessage sendMessageTask = new SendMessage();
+        sendMessageTask.execute();
+    }
+
+    public void sendMessage(View v)
+    {
+        try {
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            out.writeUTF("TEST");
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void showText(String s)
     {
         EditText editText = (EditText) findViewById(R.id.chatPane);
         editText.append(s);
+    }
+
+    private class SendMessage extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                client = new Socket("10.0.0.3", 8189); // connect to the server
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                while(true) {
+                    in.readUTF();
+                }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
 }
